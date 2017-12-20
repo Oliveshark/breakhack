9250dad9cea4d6e7bb3dfedc49d3cf5b30e96b0c
Linus Probert <linus.probert@gmail.com>
2017-12-20 19:17:41 +0100

    Get screen resolution in Windows

--- src/screenresolution.c
+++ src/screenresolution.c
@@ -2,6 +2,8 @@
 
 #ifndef _WIN32
 #include <X11/Xlib.h>
+#else // _WIN32
+#include <windows.h>
 #endif // _WIN32
 #include <stdlib.h>
 
@@ -17,7 +19,7 @@ Dimension getScreenDimensions(void)
 	free(d);
 	free(s);
 #else // _WIN32
-	Dimension dim = (Dimension) { 1920, 1080 };
+	Dimension dim = (Dimension) { GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN) };
 #endif // _WIN32
 
 	return dim;
