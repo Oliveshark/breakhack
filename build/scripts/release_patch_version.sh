f049d6621951abca67016fbde348e3f621618255
Linus Probert <linus.probert@verisure.com>
2018-05-17 10:06:58 +0200

    Fixed releasenotes and script again

--- build/scripts/release_patch_version.sh
+++ build/scripts/release_patch_version.sh
@@ -4,16 +4,19 @@
 LAST_VERSION=$(egrep -o 'breakhack_PATCH_VERSION [0-9]+' CMakeLists.txt | awk '{print $2}')
 NEXT_VERSION=$((LAST_VERSION + 1))
 
+LAST_TAG=early-access-v$LAST_VERSION
+NEXT_TAG=early-access-v$NEXT_VERSION
+
 # Update the version and create release notes
 sed -i -e "s/breakhack_PATCH_VERSION [0-9]\+/breakhack_PATCH_VERSION $NEXT_VERSION/" CMakeLists.txt
-git log --oneline early-access-v$LAST_VERSION..early-access-v$NEXT_VERSION > build/releasenotes/early-access-v$NEXT_VERSION
-git add build/releasenotes/early-access-v$NEXT_VERSION
+git log --oneline $LAST_TAG..$NEXT_TAG > build/releasenotes/$NEXT_TAG.txt
+git add build/releasenotes/$NEXT_TAG.txt
 git commit -a -m"Patch version raised to $NEXT_VERSION"
+
+# Create the tag
+git tag $NEXT_TAG
 
 # Push to repo
 git push
-
-# Create the tag
-git tag early-access-v$NEXT_VERSION
-git push origin early-access-v$NEXT_VERSION
+git push origin $NEXT_TAG
 
