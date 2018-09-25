bc452a3fb61cdd27c24e761741b9cafa07b3c5fb
Linus Probert <linus.probert@gmail.com>
2018-09-25 14:26:50 +0200

    Patch version raised to

--- build/scripts/release_patch_version.sh
+++ build/scripts/release_patch_version.sh
@@ -4,7 +4,7 @@
 MAJOR_VERSION=$(egrep -o 'breakhack_MAJOR_VERSION [0-9]+' CMakeLists.txt | awk '{print $2}')
 MINOR_VERSION=$(egrep -o 'breakhack_MINOR_VERSION [0-9]+' CMakeLists.txt | awk '{print $2}')
 LAST_PATCH_VERSION=$(egrep -o 'breakhack_PATCH_VERSION [0-9]+' CMakeLists.txt | awk '{print $2}')
-NEXT_PATCH_VERSION=$((LAST_VERSION + 1))
+NEXT_PATCH_VERSION=$((LAST_PATCH_VERSION + 1))
 
 LAST_TAG=v$MAJOR_VERSION.$MINOR_VERSION.$LAST_PATCH_VERSION
 NEXT_TAG=v$MAJOR_VERSION.$MINOR_VERSION.$NEXT_PATCH_VERSION
