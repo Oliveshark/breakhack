5802db7a35e18d979ac35cd3d514b5821266574e
Linus Probert <linus.probert@gmail.com>
2026-03-13 12:39:50 +0100

    enforce lua formatting (#154)
    * Drop .vimrc
    
    * Format and fix lua files
    
    Adds git job to enforce lua formatting.

--- data/menumapgen.lua
+++ data/menumapgen.lua
@@ -1,7 +1,7 @@
-local room_builder = require "maproombuilder"
-local monster_gen = require "monstergen"
-local trap_gen = require "trapgen"
-local chest_gen = require "chestgen"
+local room_builder = require("maproombuilder")
+local monster_gen = require("monstergen")
+local trap_gen = require("trapgen")
+local chest_gen = require("chestgen")
 
 map = create_map(CURRENT_LEVEL) -- 'map' needs to be global
 
@@ -12,8 +12,8 @@ local room = room_builder.create_empty_room()
 room_builder.build_room(room, 0, 0)
 room_builder.add_full_lighting(room)
 monster_gen.add_monsters_to_room(room, 0, 0)
-trap_gen.add_traps_to_room(room, 0, 0)
-chest_gen.add_chests_to_room(room, 0, 0)
+trap_gen.add_traps_to_room(room)
+chest_gen.add_chests_to_room(room)
 room_builder.load_room(map, room)
 monster_gen.load_monsters(map, room.monsters)
 trap_gen.load_traps(map, room.traps)
